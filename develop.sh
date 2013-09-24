d28e05e2b6939c16866198e7694c4164d962bffa
Jim Yeh <lemonlatte@gmail.com>
2013-09-25 00:02:09 +0800

    Install saltstack with developing version
    * Remove /srv if it is existent.

--- develop.sh
+++ develop.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-wget -O - http://bootstrap.saltstack.org | sh
-if [ ! -d /srv ]; then
-    ln -s `pwd`/srv /srv
+curl -L http://bootstrap.saltstack.org | sudo sh -s -- git develop
+if [ -d /srv ]; then
+    rm -rf /srv
 fi
+ln -s `pwd`/srv /srv
 salt-call state.highstate --local
