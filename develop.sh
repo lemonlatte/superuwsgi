d7b5b3446edcfd6c527c870280b6d3ae6c88abda
Jim Yeh <lemonlatte@gmail.com>
2013-09-30 11:43:11 +0800

    Make develop script be more generalised

--- develop.sh
+++ develop.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+BASE_PATH=$(dirname $(readlink -f "$0"))
+
 apt-get -y install python-software-properties curl git
 curl -L http://bootstrap.saltstack.org | sudo sh -s -- git 5324d6
-if [ -d /srv ]; then
+
+if [ -d /srv ] || [ -L /srv ]; then
     rm -rf /srv
 fi
-ln -s `pwd`/srv /srv
+ln -s $BASE_PATH/srv /srv
+
 salt-call state.highstate --local
-# salt-call state.sls webapp.uwsgi --local
